33af8c6a3b78ddefc74e0d022648765c6a37fe7d
JulianThom <dieu.julian@gmail.com>
2017-11-05 00:35:11 +0100

    style timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import tomato from './tomato.png';
 import './App.css';
 
 class PomodoroTimer extends Component {
@@ -38,9 +38,22 @@ class PomodoroTimer extends Component {
 
   render() {
     return (
-      <div>This timer run for {this.props.workingTime} minutes, followed by rest of {this.props.restingTime} minutes.
-        <br/>For a total of {this.totalTime(this.props.workingTime, this.props.restingTime)} minutes.
-        <br/>There are {this.state.timeElapsed} secondes elapsed.
+      <div className="
+        col-xs-4 col-sm-4 col-md-4 col-lg-4
+        col-xs-offset-4 col-sm-offset-4 col-md-offset-4 col-lg-offset-4
+        wrapper-img">
+        <div className="col-lg-12 wrapper-img">
+          <img src={tomato}></img>
+        </div>
+        <div className="col-lg-12 wrapper-content">
+          <p>This timer run for {this.props.workingTime} minutes,
+          <br/>followed by rest of {this.props.restingTime} minutes.</p>
+          <br/>For a total of {this.totalTime(this.props.workingTime, this.props.restingTime)} minutes.
+          <br/>There are {this.state.timeElapsed} secondes elapsed.
+          <footer>
+            <button className="btn btn-primary">Start</button>
+          </footer>
+        </div>
       </div>
     )
   }
